9b756b7894174ef888c75f18f57eba2609c4a76c
Miguel Grado <mgradob@gmail.com>
2016-01-12 23:18:24 -0700

    Tested web form.

--- main.py
+++ main.py
@@ -14,20 +14,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import cgi
 from google.appengine.api import users
 import webapp2
+
+MAIN_PAGE_HTML = """\
+<html>
+  <body>
+    <form action="/sign" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
+  </body>
+</html>
+"""
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
+        self.response.write(MAIN_PAGE_HTML)
 
-        if user:
-            self.response.write('Hello ' + user.nickname())
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        self.response.write('<html><body>You wrote:<pre>')
+        self.response.write(cgi.escape(self.request.get('content')))
+        self.response.write('</pre></body></html>')
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/sign', Guestbook)
 ], debug=True)
